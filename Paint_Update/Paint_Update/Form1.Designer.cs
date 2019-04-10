d73ed71b031fa58455133fd5eedb3d9ee7873cd7
CoKhanh <duongcokhanh17110315@gmail.com>
2019-04-10 21:32:02 +0700

    Submit new Source Code
    Paint for Window Programming (First step)

---
+++ Paint_Update/Paint_Update/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace Paint_Update
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.undoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.howToUseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.howToUseToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.versionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRe = new System.Windows.Forms.Button();
+            this.btnClr = new System.Windows.Forms.Button();
+            this.btnFEllip = new System.Windows.Forms.Button();
+            this.btnFRe = new System.Windows.Forms.Button();
+            this.btnDEllip = new System.Windows.Forms.Button();
+            this.btnDRe = new System.Windows.Forms.Button();
+            this.btnDLine = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.howToUseToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(128, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.newToolStripMenuItem.Text = "&New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.undoToolStripMenuItem,
+            this.redoToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "&Edit";
+            this.editToolStripMenuItem.Click += new System.EventHandler(this.editToolStripMenuItem_Click);
+            // 
+            // undoToolStripMenuItem
+            // 
+            this.undoToolStripMenuItem.Name = "undoToolStripMenuItem";
+            this.undoToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.undoToolStripMenuItem.Text = "&Undo";
+            // 
+            // redoToolStripMenuItem
+            // 
+            this.redoToolStripMenuItem.Name = "redoToolStripMenuItem";
+            this.redoToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.redoToolStripMenuItem.Text = "&Redo";
+            // 
+            // howToUseToolStripMenuItem
+            // 
+            this.howToUseToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.howToUseToolStripMenuItem1,
+            this.versionToolStripMenuItem});
+            this.howToUseToolStripMenuItem.Name = "howToUseToolStripMenuItem";
+            this.howToUseToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.howToUseToolStripMenuItem.Text = "&Help";
+            this.howToUseToolStripMenuItem.Click += new System.EventHandler(this.howToUseToolStripMenuItem_Click);
+            // 
+            // howToUseToolStripMenuItem1
+            // 
+            this.howToUseToolStripMenuItem1.Name = "howToUseToolStripMenuItem1";
+            this.howToUseToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.howToUseToolStripMenuItem1.Text = "H&ow to use";
+            this.howToUseToolStripMenuItem1.Click += new System.EventHandler(this.howToUseToolStripMenuItem1_Click);
+            // 
+            // versionToolStripMenuItem
+            // 
+            this.versionToolStripMenuItem.Name = "versionToolStripMenuItem";
+            this.versionToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.versionToolStripMenuItem.Text = "&Version";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnRe);
+            this.panel1.Controls.Add(this.btnClr);
+            this.panel1.Controls.Add(this.btnFEllip);
+            this.panel1.Controls.Add(this.btnFRe);
+            this.panel1.Controls.Add(this.btnDEllip);
+            this.panel1.Controls.Add(this.btnDRe);
+            this.panel1.Controls.Add(this.btnDLine);
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 67);
+            this.panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(601, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Color:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(456, 25);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(420, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Size:";
+            // 
+            // btnRe
+            // 
+            this.btnRe.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRe.Location = new System.Drawing.Point(722, 3);
+            this.btnRe.Name = "btnRe";
+            this.btnRe.Size = new System.Drawing.Size(75, 61);
+            this.btnRe.TabIndex = 6;
+            this.btnRe.Text = "Refresh";
+            this.btnRe.UseVisualStyleBackColor = true;
+            this.btnRe.Click += new System.EventHandler(this.btnRe_Click);
+            // 
+            // btnClr
+            // 
+            this.btnClr.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClr.BackColor = System.Drawing.Color.Black;
+            this.btnClr.Location = new System.Drawing.Point(641, 3);
+            this.btnClr.Name = "btnClr";
+            this.btnClr.Size = new System.Drawing.Size(75, 61);
+            this.btnClr.TabIndex = 5;
+            this.btnClr.UseVisualStyleBackColor = false;
+            this.btnClr.Click += new System.EventHandler(this.btnClr_Click);
+            // 
+            // btnFEllip
+            // 
+            this.btnFEllip.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFEllip.Location = new System.Drawing.Point(327, 3);
+            this.btnFEllip.Name = "btnFEllip";
+            this.btnFEllip.Size = new System.Drawing.Size(75, 61);
+            this.btnFEllip.TabIndex = 4;
+            this.btnFEllip.Text = "Fill Ellipse";
+            this.btnFEllip.UseVisualStyleBackColor = true;
+            // 
+            // btnFRe
+            // 
+            this.btnFRe.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFRe.Location = new System.Drawing.Point(246, 3);
+            this.btnFRe.Name = "btnFRe";
+            this.btnFRe.Size = new System.Drawing.Size(75, 61);
+            this.btnFRe.TabIndex = 3;
+            this.btnFRe.Text = "Fill Retangle";
+            this.btnFRe.UseVisualStyleBackColor = true;
+            // 
+            // btnDEllip
+            // 
+            this.btnDEllip.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDEllip.Location = new System.Drawing.Point(165, 3);
+            this.btnDEllip.Name = "btnDEllip";
+            this.btnDEllip.Size = new System.Drawing.Size(75, 61);
+            this.btnDEllip.TabIndex = 2;
+            this.btnDEllip.Text = "Draw Ellipse";
+            this.btnDEllip.UseVisualStyleBackColor = true;
+            // 
+            // btnDRe
+            // 
+            this.btnDRe.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDRe.Location = new System.Drawing.Point(84, 3);
+            this.btnDRe.Name = "btnDRe";
+            this.btnDRe.Size = new System.Drawing.Size(75, 61);
+            this.btnDRe.TabIndex = 1;
+            this.btnDRe.Text = "Draw Retangle";
+            this.btnDRe.UseVisualStyleBackColor = true;
+            // 
+            // btnDLine
+            // 
+            this.btnDLine.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDLine.Location = new System.Drawing.Point(3, 3);
+            this.btnDLine.Name = "btnDLine";
+            this.btnDLine.Size = new System.Drawing.Size(75, 61);
+            this.btnDLine.TabIndex = 0;
+            this.btnDLine.Text = "Draw Line";
+            this.btnDLine.UseVisualStyleBackColor = true;
+            this.btnDLine.Click += new System.EventHandler(this.btnDLine_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "&Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 357);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem undoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem howToUseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem howToUseToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem versionToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRe;
+        private System.Windows.Forms.Button btnClr;
+        private System.Windows.Forms.Button btnFEllip;
+        private System.Windows.Forms.Button btnFRe;
+        private System.Windows.Forms.Button btnDEllip;
+        private System.Windows.Forms.Button btnDRe;
+        private System.Windows.Forms.Button btnDLine;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+    }
+}
+
---
+++ Paint_Update/Paint_Update/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Paint_Update
+{
+    public partial class Form1 : Form
+    {
+        #region Biến toàn cục
+        private Point _p1;
+        private Point _p2;
+        private bool isDown;
+        private Bitmap _bmp;
+        private Graphics _gp;
+        private Graphics g;
+        private int x = -1;
+        private int y = -1;
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+            _bmp = new Bitmap(this.Width,this.Height);
+            _gp = Graphics.FromImage(_bmp);
+            _gp.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            this.SetStyle(ControlStyles.UserPaint, true);
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            
+        }
+        
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void btnClr_Click(object sender, EventArgs e)
+        {
+            DialogResult rs = colorDialog1.ShowDialog();
+            if(rs==DialogResult.OK)
+            {
+                btnClr.BackColor = colorDialog1.Color;
+            }
+        }
+        #region Sự kiện
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            //if (isDown)
+            //{
+            //    x = e.X;
+            //    y = e.Y;
+            //    _p1 = new Point(e.Location.X, e.Location.Y);
+            //}
+            isDown = true;
+            _p1 = new Point(e.Location.X, e.Location.Y);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(isDown)
+            {
+                _p2 = new Point(e.Location.X, e.Location.Y);
+                //Pen nPen = new Pen(btnClr.BackColor, Convert.ToInt16(numericUpDown1.Value));
+                //nPen.StartCap = nPen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+                //g.DrawLine(nPen, new Point(x, y), e.Location);
+
+                //x = e.X;
+                //y = e.Y;
+                this.Refresh();
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDown = false;
+            Pen nPen = new Pen(btnClr.BackColor, Convert.ToInt16(numericUpDown1.Value));
+            _gp.DrawLine(nPen, _p1,_p2);
+            this.BackgroundImage = (Bitmap)_bmp.Clone();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (isDown)
+            {
+                Pen nPen = new Pen(btnClr.BackColor, Convert.ToInt16(numericUpDown1.Value));
+                nPen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+                e.Graphics.DrawLine(nPen, _p1, _p2);
+            }
+        }
+
+        private void btnRe_Click(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnDLine_Click(object sender, EventArgs e)
+        {
+            this.isDown = true;
+        }
+
+        private void howToUseToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Chỉ cần bấm chọn vào các chức năng vẽ hình mà bạn muốn vẽ thôi thằng gà !!!");
+
+        }
+
+        private void howToUseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+        
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _bmp.Save(@"C:\Users\DuongCoKhanh\Desktop\Hình Game\bmp.jpg");
+        }
+        #endregion
+    }
+
+}
